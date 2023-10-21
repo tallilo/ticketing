80f2fd2439398944fa73f56fded6fb0c6f00fbdc
tallilo <tallilo5011323@gmail.com>
2023-10-21 15:31:24 +0300

    add startup message to orders service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -6,6 +6,7 @@ import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listene
 import { PaymentCreatedListener } from "./events/listeners/payment-created-listener";
 import { ExpirationCompleteListener } from "./events/listeners/expiration-complete-listener";
 const start = async () => {
+  console.log("Starting...");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must define");
   }
